902497af58876243d2c47493d8069a2bce3baab0
longzhitengye <2286313862@qq.com>
2022-09-14 16:24:12 +0800

    del s

--- bricks/clean_archi/__brick__/{{module_name}}/domain/usecases/{{module_name}}_use_cases.dart
+++ bricks/clean_archi/__brick__/{{module_name}}/domain/usecases/{{module_name}}_use_cases.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 
 
-abstract class Get{{module_name.pascalCase()}}Model implements  UseCase<{{module_name.pascalCase()}}Model, NoParams> {
-  Future<Either<Failure, {{module_name.pascalCase()}}Model>> call(NoParams params);
+abstract class Get{{module_name.pascalCase()}}Model implements  UseCase<{{module_name.pascalCase()}}Model, NoParam> {
+  Future<Either<Failure, {{module_name.pascalCase()}}Model>> call(NoParam params);
 }
